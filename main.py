7618588b3bac6e952c1977031aa96324a736a07e
Thibault Lombart <lombart.thibault@gmail.com>
2023-10-06 14:17:02 +0200

    Ajout templates

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from flask import Flask, render_template
 
+import requests
+
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired, Length
+
 app = Flask(__name__)
 app.secret_key = 'Ma clé secrète'
+
+
+class PokemonForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
+    codepostal = StringField('codepostal', validators=[DataRequired(), Length(max=5)])
+
 
 @app.route('/')
 def index():
